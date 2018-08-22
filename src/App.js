0544ab2635c22075b648e1432da3b8197f4110b3
Ruth Ashleigh <notruth@notruth.com>
2018-08-22 14:24:40 -0700

    Added snake movement func, outlines for other game mechanic functions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
   state = {
     gameBoard: [],
     snakeLength: 5,
+    currentDirection: "right",
+    snakePosition: [],
   }
 
   componentDidMount(){
@@ -33,16 +35,124 @@ class App extends Component {
     //copy the current state gameBoard
     let gameBoard = [...this.state.gameBoard]
     let centralCell = 12;
+    let snakePosition = [];
     //starting from central cell (column 12) running snakeLen times
     for (let i = centralCell; i > (centralCell - this.state.snakeLength); i--){
       //set cell value to "snake"
       gameBoard[13][i] = "snake";
+      console.log(i)
+      // add that cell to the snakes position array to be updated in state
+      snakePosition.push([13, i])
     }
+      console.log(JSON.parse(JSON.stringify(snakePosition.slice())));
 
     this.setState({
       gameBoard: gameBoard,
+      snakePosition: snakePosition
     })
 
+  }
+
+  validMove = (row, col) => {
+    // if the move is within board boundary
+    if(row >= 0 && row < 26 && col >=0 && col <25){
+      return true;
+    }
+    // if returning false, player has run into wall
+    return false;
+  }
+
+  collidedWithSelf = (row, col) => {
+    let gameBoard = [...this.state.gameBoard];
+    if (gameBoard[row][col] === "snake"){
+      this.playerHasDied()
+    }
+  }
+
+  playerHasDied = () => {
+    console.log("died!")
+  }
+
+  changeDirection = (direction) =>{
+    this.setState({
+      currentDirection: direction,
+    })
+  }
+
+  moveSnake = (direction) => {
+    // get array of current snake positions
+    let snakePosition = [...this.state.snakePosition];
+    console.log("starting move")
+
+
+    let gameBoard = [...this.state.gameBoard];
+    // get position of head
+    let snakeHeadRow = snakePosition[0][0]
+    let snakeHeadCol = snakePosition[0][1]
+    // make empty array to store the snakes new position after successful move
+    let snakeNextPosition = [];
+    //let incrementRow = 0;
+    //let incrementCol = 0;
+
+    switch(direction) {
+      case "right":
+          snakeNextPosition = [snakeHeadRow, snakeHeadCol+1]
+          //incrementRow = 0
+          //incrementCol = 1
+          break;
+      case "left":
+          snakeNextPosition = [snakeHeadRow, snakeHeadCol-1]
+          //incrementRow = 0
+          //incrementCol = -2
+          break;
+      case "up":
+          snakeNextPosition = [snakeHeadRow-1, snakeHeadCol]
+          //incrementRow = -2
+          //incrementCol = 0
+          break;
+      case "down":
+          snakeNextPosition = [snakeHeadRow+1, snakeHeadCol]
+          //incrementRow = 1
+          //incrementCol = 0
+          break;
+      default:
+          snakeNextPosition = [snakeHeadRow, snakeHeadCol]
+    }
+
+    // if the snakes new head position is valid, delete the last cell from the snakes old position
+    if (this.validMove(snakeNextPosition[0], snakeNextPosition[1])){
+      //delete the tail
+      snakePosition.pop()
+      //then add the new head to the front
+      snakePosition.unshift(snakeNextPosition)
+
+      // then loop through the old positions incrememnting each by one depending on direction
+      /*snakePosition = snakePosition.map((positionArr) => {
+        positionArr[0] = positionArr[0] + incrementRow
+        positionArr[1] = positionArr[1] + incrementCol
+        return positionArr
+      })*/
+
+      console.log(JSON.parse(JSON.stringify(snakePosition.slice())));
+
+
+      // figure out how to update gameBoard based on new snake array
+
+
+      // finally update the snakePosition and gameBoard states with the new values
+      this.setState({
+        snakePosition: snakePosition,
+        gameBoard: gameBoard,
+      })
+    } else {
+      // if move was invalid
+      return this.playerHasDied()
+    }
+
+  }
+
+  startGame = () =>{
+    this.moveSnake("right")
   }
 
   render() {
@@ -50,6 +160,7 @@ class App extends Component {
       <div className="App">
         <Grid gameBoard={this.state.gameBoard}/>
         <button onClick={this.placeStartingSnake}>Add snake</button>
+        <button onClick={this.startGame}>Start</button>
       </div>
     );
   }
